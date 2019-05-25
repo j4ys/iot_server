cff9ee636bb122aa224294a04d61db3e9ed572ae
j4ys <j4y1302@gmail.com>
2019-05-25 23:37:23 +0530

    add support for env file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { ApolloServer, gql } from "apollo-server-express";
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 import { typeDefs } from "./typeDefs";
 import { resolvers } from "./resolvers";
@@ -8,7 +11,7 @@ import { resolvers } from "./resolvers";
 const startServer = async () => {
   const app = express();
 
-  await mongoose.connect("mongodb://localhost:27017/zim", {
+  await mongoose.connect(process.env.DB_URI, {
     useNewUrlParser: true
   });
   const server = new ApolloServer({
